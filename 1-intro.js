4f039fdfc67ea3d24612325508eaef3290c2b0aa
Aruna Nuwan <arunanuwan5413044@gmail.com>
2025-02-05 07:35:24 +0530

    Create Modules Intro folder and 1-intro.js file

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const number = Math.floor(Math.random() * 10);
+
+if (number < 5) {
+    console.log(number + ' - ' + ' Number is below 5');
+} else {
+    console.log(number + ' - ' + ' Number is above 5');
+}
+
+console.log('This is my first node app!');
